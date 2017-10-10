75c02631f0b46516b9547e1210f6472ba19c8b18
Andrew Miller <generalandrew@gmail.com>
2017-10-10 11:31:28 -0600

    feat(server): move HMR into dev node_env via config

--- server/index.js
+++ server/index.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const path    = require('path')
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
+const express = require('express');
+const path    = require('path');
 
-const app = express()
-const config = require('../tools/webpack/webpack.config.js')
-const compiler = webpack(config)
+const config = require('../config');
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath
-}))
+const app = express();
 
-app.use(webpackHotMiddleware(compiler))
+if (config.env === 'development') {
+  const webpackConfig = require('../tools/webpack/webpack.config.js');
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath
+  }));
+
+  app.use(webpackHotMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath
+  }));
+}
 
 app.listen(3000, function(err) {
   if (err) {
     return console.log(err);
   }
 
-  console.log('listening on port')
-})
+  console.log('listening on port');
+});
